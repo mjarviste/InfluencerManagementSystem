59c7b7b7c8740f4f0618a4dadc38000c90652c65
Mihkel Järviste <mjarviste@gmail.com>
2024-12-30 17:28:26 +0200

    fix: Update package.json for api deployment

--- api/src/index.ts
+++ api/src/index.ts
@@ -8,7 +8,9 @@ import accountRouter from './routes/account.routes';
 dotenv.config();
 
 const app = express();
-app.use(cors({origin: process.env.CLIENT_URL, credentials: true}))
+
+const CLIENT_URL = process.env.CLIENT_URL || '*';
+app.use(cors({origin: CLIENT_URL, credentials: true}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
